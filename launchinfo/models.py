b8049ecb19dbed1ddf8e912bcc40bda5fcfeedb5
Ryan <meznokto@gmail.com>
2025-07-11 17:50:39 -0400

    Added comments to ignore warnings in vscode.

--- launchinfo/models.py
+++ launchinfo/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-from django.utils import timezone
+from django.db import models # type: ignore
+from django.utils import timezone # type: ignore
 
 from waterinfo.models import Water
 
