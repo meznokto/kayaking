c15d51db921e9c2c47db65490b155c8d9722e602
Ryan <meznokto@gmail.com>
2025-07-27 15:45:12 -0400

    Cleaned up field(s) parameter handling.

--- waterinfo/views.py
+++ waterinfo/views.py
@@ -48,17 +48,18 @@ class WaterAPI(APIView):
         if 'fields' in request.GET:
             if request.GET['fields'] == 'all':
                 fields = None
-        else:
-            if 'field' in request.GET:
+        elif 'field' in request.GET:
                 if request.GET['field'] == 'all':
-                    # should we check and not allow all fields if
-                    # no water is specified, to avoid basically dumping
-                    # the database?
                     fields = None  # return all fields
-                else:
-                    # if specific fields are requested, filter them
-                    fields = request.GET.getlist('field')
-                    fields.append('id') # always include the ID field
+
+        if not 'fields' in locals():
+            if 'fields' in request.GET:
+                # if specific fields are requested, filter them
+                fields = request.GET.get('fields').split(',')
+                fields.append('id') # always include the ID field
+            elif 'field' in request.GET:
+                fields = request.GET.get('field').split(',')
+                fields.append('id')
             else:
                 # if no fields are specified, return a default set
                 # this is useful for listing launches in a compact format
